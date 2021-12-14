ab55953a9a6c401c9f8fe32c5cfb4cd480d47de6
egri-richard <egri.dani.r@gmail.com>
2021-12-14 14:20:01 +0100

    fetch finished + nice styling

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <User v-for="d in this.data" :key="d.name" :info="d" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import User from './components/User.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    User
+  },
+  data() {
+    return {
+      data: Array
+    }
+  },
+  created() {
+    this.getUsers()
+  },
+  methods: {
+      async getUsers() {
+        try {
+            const res = await fetch('http://10.4.18.51:8080/helloWorld')
+            const data = await res.json()
+            this.data = data
+        } catch(error) {
+          console.log(error)
+        }
+      }
   }
 }
+
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/User.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="user">
+    <h1>{{ info.name }}</h1>
+    <h3>{{ info.gender }}</h3>
+    <h3>{{ info.age }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: {info: Object}
+}
+</script>
+
+<style lang="css" scoped>
+.user {
+  display: inline-block;
+  width: 100px;
+  height: 160px;
+  margin: 10px 10px 10px 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  box-shadow: 10px 10px 10px grey;
+  background-color: rgb(245, 245, 245);
+  transition: 0.2s;
+}
+
+.user:hover {
+  transform: scale(1.1);
+}
+
+</style>
